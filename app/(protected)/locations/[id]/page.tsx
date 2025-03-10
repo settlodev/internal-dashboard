21b11bf633756de82d20df82d9a1e12cb824b50b
pbijampola <patrickbijampola@gmail.com>
2025-03-10 17:16:17 +0300

    Fixed subscriptions on location subscription table

--- app/(protected)/locations/[id]/page.tsx
+++ app/(protected)/locations/[id]/page.tsx
@@ -7,7 +7,7 @@ import Unauthorized from "@/components/code/401";
 
 const LocationDetailPage = async ({params}: {params: {id: string}}) => {
     const location = await getLocation(params.id as UUID);
-    const payments = await getLocationSubscriptionPayments(params.id as UUID);
+    const payments = await getLocationSubscriptionPayments(params.id as UUID, 0, 10);
     const activeSubscription = await getActiveSubscription(params.id as UUID);
     
     return (
