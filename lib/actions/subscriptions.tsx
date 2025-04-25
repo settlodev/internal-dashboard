5161785413ff1846eb3d946eaffdb23d4ac6a2a5
PATRICK BIJAMPOLA <patrickbijampola@gmail.com>
2025-04-25 20:02:13 +0300

    Update subscriptions.tsx

--- lib/actions/subscriptions.tsx
+++ lib/actions/subscriptions.tsx
@@ -9,7 +9,7 @@ export const getAllSubscriptions = async (): Promise<Payment[]> => {
         const apiClient = new ApiClient();
         const payload ={
             "page": 0,
-            "size": 1000
+            "size": 10000
         }
         const data = await apiClient.post<ApiResponse<Payment[]>, {}>("/api/subscription-payments", payload);
         // console.log("The subscription records", data)
