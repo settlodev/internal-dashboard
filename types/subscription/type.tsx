451b88e13147dd7291c460dd43a597b0c6ca7434
PATRICK BIJAMPOLA <patrickbijampola@gmail.com>
2025-12-19 12:41:51 +0300

    fix(subscriber functinality, added new data types)

--- types/subscription/type.tsx
+++ types/subscription/type.tsx
@@ -14,4 +14,18 @@ export interface SubscriberData {
     inactiveSubscribers: number;
     churnedSubscribers: number;
     newTotalUsers:number;
+    "newTotalLocations": number,
+    "totalLocationSubscriptions": number,
+    "activeLocationSubscriptions": number,
+    "inactiveLocationSubscriptions": number,
+    "newLocationSubscriptions": number,
+    "renewedLocationSubscriptions": number,
+    "churnedLocationSubscriptions": number,
+    "newTotalWarehouses": number,
+    "totalWarehouseSubscriptions": number,
+    "activeWarehouseSubscriptions": number,
+    "inactiveWarehouseSubscriptions": number,
+    "renewedWarehouseSubscriptions": number,
+    "newWarehouseSubscriptions": number,
+    "churnedWarehouseSubscriptions": number
 }
